58f848320009cee31183fd2c786a523a1a708920
ahmed-menisy <amelmaster16@gmail.com>
2023-01-19 13:07:48 +0200

    upload 2

--- src/app/Compnents/compnents/frist-swiper/frist-swiper.component.ts
+++ src/app/Compnents/compnents/frist-swiper/frist-swiper.component.ts
@@ -18,83 +18,83 @@ export class FristSwiperComponent {
     this.images = [
       {
         category: 'fashion',
-        src: './../../../assets/carousel2/1.png',
+        src: './assets/carousel2/1.png',
       },
       {
         category: 'summer',
-        src: './../../../assets/carousel2/2.png',
+        src: './assets/carousel2/2.png',
       },
       {
         category: 'beauty',
-        src: './../../../assets/carousel2/3.png',
+        src: './assets/carousel2/3.png',
       },
       {
         category: 'clearance',
-        src: './../../../assets/carousel2/5.png',
+        src: './assets/carousel2/5.png',
       },
       {
         category: 'beauty',
-        src: './../../../assets/carousel2/6.png',
+        src: './assets/carousel2/6.png',
       },
       {
         category: 'home',
-        src: './../../../assets/carousel2/7.png',
+        src: './assets/carousel2/7.png',
       },
       {
         category: 'fashion',
-        src: './../../../assets/carousel2/8.png',
+        src: './assets/carousel2/8.png',
       },
       {
         category: 'fashion',
-        src: './../../../assets/carousel2/9.png',
+        src: './assets/carousel2/9.png',
       },
       {
         category: 'mobiles',
-        src: './../../../assets/carousel2/10.png',
+        src: './assets/carousel2/10.png',
       },
       {
         category: 'wearables',
-        src: './../../../assets/carousel2/11.png',
+        src: './assets/carousel2/11.png',
       },
       {
         category: 'fragrances',
-        src: './../../../assets/carousel2/12.png',
+        src: './assets/carousel2/12.png',
       },
       {
         category: 'TVs',
-        src: './../../../assets/carousel2/13.png',
+        src: './assets/carousel2/13.png',
       },
       {
         category: 'electronics',
-        src: './../../../assets/carousel2/14.png',
+        src: './assets/carousel2/14.png',
       },
       {
         category: 'grocery',
-        src: './../../../assets/carousel2/15.png',
+        src: './assets/carousel2/15.png',
       },
       {
         category: 'health',
-        src: './../../../assets/carousel2/16.png',
+        src: './assets/carousel2/16.png',
       },
       {
         category: 'headphones',
-        src: './../../../assets/carousel2/17.png',
+        src: './assets/carousel2/17.png',
       },
       {
         category: 'laptops',
-        src: './../../../assets/carousel2/18.png',
+        src: './assets/carousel2/18.png',
       },
       {
         category: 'sports',
-        src: './../../../assets/carousel2/19.png',
+        src: './assets/carousel2/19.png',
       },
       {
         category: 'fragrances',
-        src: './../../../assets/carousel2/20.png',
+        src: './assets/carousel2/20.png',
       },
       {
         category: 'appliances',
-        src: './../../../assets/carousel2/21.png',
+        src: './assets/carousel2/21.png',
       },
     ];
   }
